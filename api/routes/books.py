62314bbf2bf1d988b162578b7ba3de219ca0a561
Muturi-002 <martinmnjoroge03@gmail.com>
2025-02-11 14:17:04 +0300

    Modified books.py, adding an endpoint for retrieving book by any ID

--- api/routes/books.py
+++ api/routes/books.py
@@ -2,6 +2,7 @@ from typing import OrderedDict
 
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, status
 
 from api.db.schemas import Book, Genre, InMemoryDB
 
@@ -60,3 +61,11 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int) -> Book: #gets value from the Book 'struct'(definition in Go) in the db.schemas.py file
+    #the 'async' keyword allows for concurrency in this function.'await' ensures that the function waits for the response before proceeding
+    book = await db.get_book(book_id)
+    if book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book does not exist")
+    return book
